0bfeed90c142c2d33965ac45d2bbe6e2a09a7851
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-03-27 17:41:34 -0300

    feat: Adding delete and update username options

--- beck-end/routes/calls.js
+++ beck-end/routes/calls.js
@@ -44,7 +44,7 @@ router.post('/login', async(req, res) => {
     };
 });
 
-router.patch('/profile/:id', async(req, res) => {
+router.patch('/profilePassword/:id', async(req, res) => {
     try{
         const user_id  = req.params.id; // TODO Something will have to keep user_id
         const { user_password } = req.body;
@@ -56,6 +56,34 @@ router.patch('/profile/:id', async(req, res) => {
     res.status(200).json({message: "Password changed successfully!", user: result[0]});
     }catch (error){
         console.log("Update error", error);
+        res.status(500).json({error: "ERROR!"});
+    };
+});
+router.patch('/profileUsername/:id', async(req, res) => { // username change
+    try{
+        const user_id = req.params.id;
+        const { username } = req.body;
+        const [ result ] = await connection.execute(
+            "update social set username=? where user_id=?",
+            [ username, user_id]
+        );
+    res.status(200).json({message: "Usernaeme changed successfully", user: result[0]});
+    }catch (error){
+        console.log("Username update error", error);  
+        res.status(500).json({error: "ERROR!"});  
+    };
+});
+
+router.delete('/delete/:id', async(req, res) => {
+    try{
+        const user_id = req.params.id;
+        const [ result ] = await connection.execute(
+            "delete from social where user_id=?",
+            [ user_id ]
+        );
+    res.status(200).json({message: "User deleted successfully!", user: result[0]});
+    }catch (error){
+        console.log("Delete failled");
         res.status(500).json({error: "ERROR!"});
     };
 });
