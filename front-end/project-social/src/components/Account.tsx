57ee179e5fbe4bc1d3eab92d9024d995bc13eac6
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-03-28 13:44:35 -0300

    feat: Adding change buttons

--- front-end/project-social/src/components/Account.tsx
+++ front-end/project-social/src/components/Account.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Account.css";
-import profilePic from "/home/noSecureOption/Projects/Social/front-end/project-social/profilePic.png"
+import profilePic from "/home/noSecureOption/Projects/Social/front-end/project-social/profilePic.png";
 import { useNavigate } from "react-router-dom";
 
 function AccountPage() {
@@ -19,35 +19,40 @@ function AccountPage() {
   return (
     <div className="account-page">
       <div className="account-box">
-          <button className="btn btn-success btn-back-home"
-            onClick={changeRoute}
-            > {`<`} Home</button>
+        <button className="btn btn-success btn-back-home" onClick={changeRoute}>
+          {" "}
+          {`<`} Home
+        </button>
         <div className="photo-container">
-        <img
-            src={profilePic}
-            alt="Profile"
-            className="profile-photo"
-          />
+          <img src={profilePic} alt="Profile" className="profile-photo" />
+        </div>
+      </div>
+
+      <div className="credencials">
+        <div className="info-row-user-login">
+          <label className="label">user_login:</label>
+          <span className="value">{account.user_login}</span>
         </div>
 
-        <div className="credencials">
-            <div className="info-row">
-            <label className="label">Username:</label>
-            <span className="value">{account.username}</span>
-            </div>
-
-            <div className="info-row">
-            <label className="label">user_login:</label>
-            <span className="value">{account.user_login}</span>
-            </div>
-
-            <div className="info-row">
-            <label className="label">Password:</label>
-            <span className="value">
-                {"*".repeat(account.user_password.length)}
-            </span>
-            </div>
+        <div className="info-row">
+          <label className="label">Username:</label>
+          <span className="value">{account.username}</span>
         </div>
+
+        <button type="button" className="btn btn-dark text-dark">
+          Change
+        </button>
+
+        <div className="info-row">
+          <label className="label">Password:</label>
+          <span className="value">
+            {"*".repeat(account.user_password.length)}
+          </span>
+        </div>
+
+        <button type="button" className="btn btn-dark text-dark">
+          Change
+        </button>
       </div>
     </div>
   );
