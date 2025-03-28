886a1216397f1eeb73050c7e679a9e4dd0906d20
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-03-28 10:51:36 -0300

    style: Improving post bar style

--- front-end/project-social/src/components/Home.tsx
+++ front-end/project-social/src/components/Home.tsx
@@ -1,48 +1,61 @@
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-
+import musicIcon from "/home/noSecureOption/Projects/Social/front-end/project-social/music-note-beamed.svg";
+import emojiIcon from "/home/noSecureOption/Projects/Social/front-end/project-social/emoji-smile.svg";
+import photoIcon from "/home/noSecureOption/Projects/Social/front-end/project-social/image.svg";
 
 function Home() {
-
   const navigate = useNavigate();
 
   const changeRouteToAccount = () => {
-    navigate("/account")
-  }
+    navigate("/account");
+  };
 
   const changeRouteToLogin = () => {
     navigate("/login");
-  }
+  };
   return (
     <>
       <div className="fixedContent">
         <div>
-          <button className="profile-box">
-            <span className="text-white fw-bold"
-              onClick={changeRouteToAccount}
-            >PROFILE</span>
+          <button type="button" className="btn profile-box">
+            <span className="text-black fw-bold" onClick={changeRouteToAccount}>
+              PROFILE
+            </span>
           </button>
-          <button className="settings-box">
-            <span className="text-white fw-bold">SETTINGS</span>
+
+          <button type="button" className="btn settings-box">
+            <span className="text-black fw-bold">SEARCH</span>
           </button>
         </div>
 
         <div>
-          <button className="logout-box">
-            <span className="text-white fw-bold"
-              onClick={changeRouteToLogin}
-            >LOGOUT</span>
+          <button type="button" className="btn logout-box">
+            <span className="text-black fw-bold" onClick={changeRouteToLogin}>
+              LOGOUT
+            </span>
           </button>
         </div>
       </div>
 
       <main>
-        <div className="search-bar">
-          <input 
-          id="search"
-          type="text"
-          maxLength={50}
-          placeholder="Buscar usuario"
+        <div className="post-pit">
+          <div className="post-pit-bottom">
+            
+            <button type="button" className="btn" ><img src={emojiIcon} alt="Emoji" /></button>
+            <button type="button" className="btn"><img src={photoIcon} alt="Photo" /></button>
+            <button type="button" className="btn"><img src={musicIcon} alt="Music" /></button>
+            <button type="button" className="btn btn-success post-pit-bottom-button">
+              Post!
+            </button>
+          </div>
+
+          <input
+            className="post-bar"
+            id="post"
+            type="text"
+            maxLength={300}
+            placeholder="How you feeling today?"
           ></input>
         </div>
 
