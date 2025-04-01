9b704a9abb73899a0baf7044900dc59a3751d682
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-03-31 23:13:46 -0300

    style: Improving style and code format

--- front-end/project-social/src/components/Account.tsx
+++ front-end/project-social/src/components/Account.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./Account.css";
 import profilePic from "/home/noSecureOption/Projects/Social/front-end/project-social/profilePic.png";
 import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.js";
 
 function AccountPage() {
   const [account] = useState({
@@ -20,7 +21,6 @@ function AccountPage() {
     <div className="account-page">
       <div className="account-box">
         <button className="btn btn-success btn-back-home" onClick={changeRoute}>
-          {" "}
           {`<`} Home
         </button>
         <div className="photo-container">
@@ -50,9 +50,65 @@ function AccountPage() {
           </span>
         </div>
 
-        <button type="button" className="btn btn-dark text-dark">
+        <button
+          type="button"
+          className="btn btn-dark text-dark"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
           Change
         </button>
+
+        {/* <!-- Modal --> */}
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Change your password
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <input
+                  id="password"
+                  type="password"
+                  maxLength={20}
+                  placeholder="New password"
+                ></input>
+                <input
+                  id="password"
+                  type="password"
+                  maxLength={20}
+                  placeholder="Confirm new password"
+                ></input>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary text-dark"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary text-dark">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
