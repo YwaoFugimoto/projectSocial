16e7203afa8b910a602af652242f0f8e9697c96b
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-03 16:24:39 -0300

    feat: Adding authentication config and improving code format

--- beck-end/server.js
+++ beck-end/server.js
@@ -5,56 +5,48 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-///////// PASSWORD ENCRYPT
-// const passport = require('passport')
-// const flash = require('express-flash')
-// const session = require('express-session')
+const session = require('express-session')
+const flash = require('express-flash')
+const passport = require('passport')
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 
-app.use(cors())
 app.use(express.json());
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/users');
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { sameSite: 'lax' }
+}));
 
-const mongoose = require('mongoose');
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
-app.use('/', indexRouter);
-app.use('/users', userRouter);
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
+app.use('/', indexRouter); //coming here
+app.use('/users', userRouter);
 
 const port = process.env.PORT || 3000; 
 app.listen(port);
 console.log(`server running of port ${port} `);
 
-
-///////// PASSWORD ENCRYPT
-
-// const initializePassport = require('./passport-config');
-// initializePassport(
-//     passport, 
-//     user_login => users.find(user => user.user_login === user_login ),
-//     user_id => user.find(user => user.user_id === user_id)    
-// )
-
-// app.use(flash())
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }))
-
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// app.use('/login', passport.authenticate('local', {
-//     successRedirect: '/home',
-//     failureRedirect: '/login',
-//     failureFlash: true
-// }))
-
+// function checkNotAuthenticated(req, res, next){
+//     if(req.isAuthenticated()) {
+//         res.redirect('/')
+//     }
+//     next();
+// }
