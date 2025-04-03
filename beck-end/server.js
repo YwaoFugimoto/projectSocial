b8d43baa846e71efd0996a9d63031926bc1a5708
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-03 10:32:05 -0300

    refactor: Rebuilding server configs

--- beck-end/server.js
+++ beck-end/server.js
@@ -1,8 +1,60 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
 
-const http = require('http');
-const app = require('./index');
-const port = 3000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-const server = http.createServer(app);
+///////// PASSWORD ENCRYPT
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
 
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+
+app.use(cors())
+app.use(express.json());
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+
+const port = process.env.PORT || 3000; 
+app.listen(port);
+console.log(`server running of port ${port} `);
+
+
+///////// PASSWORD ENCRYPT
+
+// const initializePassport = require('./passport-config');
+// initializePassport(
+//     passport, 
+//     user_login => users.find(user => user.user_login === user_login ),
+//     user_id => user.find(user => user.user_id === user_id)    
+// )
+
+// app.use(flash())
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+// app.use('/login', passport.authenticate('local', {
+//     successRedirect: '/home',
+//     failureRedirect: '/login',
+//     failureFlash: true
+// }))
+
