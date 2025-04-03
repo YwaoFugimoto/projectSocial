055b757e63e741c6c8a19c81f66c0eaa108af185
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-03 16:15:24 -0300

    refactor: Improving pages path

--- front-end/project-social/src/App.tsx
+++ front-end/project-social/src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignIn from './components/SignIn';
 import Home from './components/Home';
 import AccountPage from './components/Account';
 
 function App() {
+
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/account" element={<AccountPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/account" element={<AccountPage />} />
+    </Routes>
   );
 }
 
