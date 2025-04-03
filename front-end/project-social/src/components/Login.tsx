43bfa57fdd3aa075f722c1be3a5a9fe6e69d71ff
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-03 16:22:19 -0300

    feat: Adding authentication & session

--- front-end/project-social/src/components/Login.tsx
+++ front-end/project-social/src/components/Login.tsx
@@ -2,37 +2,42 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
   const [user_login, setUserLogin] = useState("");
   const [user_password, setPassword] = useState("");
 
   const navigate = useNavigate();
-  
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/social/login", {
+    const response = await fetch('http://localhost:3000/users/login', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      credentials: 'include',
       body: JSON.stringify({ user_login, user_password }),
     });
     const data = await response.json();
-    if (response.ok) navigate('/home');
-    else console.log("Login error: ", data);
+    console.log(response);
+    if(response.ok) { 
+      toast.success("Login successful", data);
+      navigate("/home");
+    }else {
+      toast.error("Login error: ", data); 
+    }
   };
 
   return (
     <>
       <div className="login-page hiddeOver">
-      <div className="about">
-        <h1 className="logoLogin text-black">
-          <span className="text-success">M</span>usify
-        </h1>
-        <p className="text-black">
-          A new way to share your favorite lyrics
-        </p>
-      </div>
+        <div className="about">
+          <h1 className="logoLogin text-black">
+            <span className="text-success">M</span>usify
+          </h1>
+          <p className="text-black">A new way to share your favorite lyrics</p>
+        </div>
         <div className="login-box">
           <h2>Login</h2>
           <form onSubmit={handleSubmit}>
@@ -61,6 +66,7 @@ function Login() {
             <button type="submit" className="login-button">
               Entrar
             </button>
+            <ToastContainer />
           </form>
           <div className="signup-link">
             <span>Não tem uma conta? </span>
@@ -71,6 +77,5 @@ function Login() {
     </>
   );
 }
-
 
 export default Login;
